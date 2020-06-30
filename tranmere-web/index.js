07ba8f2766ba9c45b4ef228c28982ba9ea59978a
chrisowen <chrisowen491@gmail.com>
2020-06-30 23:06:16 +0100

    Updates

--- tranmere-web/index.js
+++ tranmere-web/index.js
@@ -33,23 +33,23 @@ async function run () {
     utils.buildPage(seasonsView, "./templates/seasons.tpl.html",'./output/site/seasons.html' );
 
     var teams = await utils.findAllTeams(150);
-    utils.buildPage({title: "Opposition Team Index",teams: teams.body.aggregations.teams.buckets},
+
+    utils.buildPage({title: "Opposition Team Index",resultsByLetter: teams.resultsByLetter, teams:teams.results },
         "./templates/teams.tpl.html", './output/site/teams.html');
 
-    for(var i=0; i<teams.body.aggregations.teams.buckets.length; i++ ) {
-        var team = teams.body.aggregations.teams.buckets[i].key;
-        if(team != "Tranmere Rovers") {
-            var results = await utils.findAllTranmereMatchesByOpposition(team, 200);
-            var meta = utils.calculateWinsDrawsLossesFromMatchesSearch(results.body.hits.hits);
-            var teamView = {
-                title: "Matches against " + team,
-                wins: meta.wins,
-                draws: meta.draws,
-                losses: meta.losses,
-                matches: results.body.hits.hits
-            };
-            utils.buildPage(teamView, "./templates/team.tpl.html", './output/site/teams/'+team+'.html');
-        }
+    for(var i=0; i<teams.results.length; i++ ) {
+        var team = teams.results[i].key;
+        var results = await utils.findAllTranmereMatchesByOpposition(team, 200);
+        var meta = utils.calculateWinsDrawsLossesFromMatchesSearch(results.body.hits.hits);
+        var teamView = {
+            title: "Matches against " + team,
+            wins: meta.wins,
+            draws: meta.draws,
+            losses: meta.losses,
+            matches: results.body.hits.hits
+        };
+        utils.buildPage(teamView, "./templates/team.tpl.html", './output/site/teams/'+team+'.html');
+
     }
      var managersQuery = {
        index: "managers",
@@ -62,6 +62,43 @@ async function run () {
 
     utils.buildPage({title: "Managerial Records",managers: managers.body.hits.hits},
         "./templates/managers.tpl.html", './output/site/managers.html');
+
+     var playerQuery = {
+       index: "players",
+       body: {
+          "sort": ["Name"],
+          "size": 200,
+       }
+     };
+    var players = await client.search(playerQuery);
+
+    for(var i=0; i < players.body.hits.hits.length; i++) {
+        if(players.body.hits.hits[i]["_source"]["Name"]) {
+            var name = players.body.hits.hits[i]["_source"]["Name"];
+            var goals = await utils.findGoalsByPlayer(name, 250);
+            var apps = await utils.findAppsByPlayer(name, 250);
+
+            var links= await utils.getLinksByPlayer(name);
+            var stats = utils.calculateStats(apps, goals);
+            var player = {
+                title: name,
+                games: apps,
+                stats: stats,
+                goals: goals,
+                links: links.body.hits.hits
+            };
+            utils.buildPage(player,"./templates/player.tpl.html", './output/site/players/'+player.title+'.html');
+
+            for(var x=0; x < stats.seasons.length; x++) {
+                var g2 = await utils.findGoalsByPlayer(name, 250, stats.seasons[x].Season);
+                var g = await utils.findAppsByPlayer(name, 250, stats.seasons[x].Season);
+                utils.buildPage({name:name, title:name + ' record in season ' + stats.seasons[x].Season, games:g, goals: g2},"./templates/player-season.tpl.html", './output/site/player-season/'+name+'-'+stats.seasons[x].Season+'.html');
+            }
+        }
+    }
+
+    utils.buildPage({title: "Player Records",players: players.body.hits.hits},
+        "./templates/players.tpl.html", './output/site/players.html');
 
     for(var i=0; i < managers.body.hits.hits.length; i++) {
 
@@ -91,6 +128,18 @@ async function run () {
             next_season: i+1,
             previous_season: i-1,
         };
+
+        var matches = [];
+        for(var x=0; x < results.body.hits.hits.length; x++) {
+            var match = results.body.hits.hits[x]["_source"];
+            match.Opposition = match.home == "Tranmere Rovers" ? match.visitor : match.home;
+            matches.push(match);
+        }
+        utils.buildPage({matches: matches}, "./templates/season.tpl.csv", './output/site/csv/'+i+'.csv');
+        var myCSVView = {
+            matches: results.body.hits.hits,
+        };
+
         utils.buildPage(mySeasonView, "./templates/season.tpl.html", './output/site/seasons/'+i+'.html');
     }
 }
--- tranmere-web/libs/utils.js
+++ tranmere-web/libs/utils.js
@@ -75,6 +75,203 @@ module.exports = function (path, fs, Mustache,client) {
             return client.search(query);
          },
 
+         findGoalsByPlayer : async function(player, size, season) {
+            var query = {
+                index: "goals",
+                body: {
+                   "sort": ["Date"],
+                   "size": size,
+                   "query": {
+                     "bool": {
+                        "must": [
+                          {
+                              "match": {
+                                "PlayersInvolved": player
+                              }
+                          }
+                        ]
+                      }
+                   }
+                }
+              };
+            if(season) {
+                query.body.query.bool.must.push(
+                    {
+                        "match": {
+                            "Season": season
+                        }
+                    }
+                );
+            }
+            var goals = await client.search(query);
+            var goalsList = [];
+            for(var i=0; i < goals.body.hits.hits.length; i++){
+                var goal = goals.body.hits.hits[i]["_source"];
+                if(goal.Scorer == player) {
+                    goal.isGoal = true;
+                    if(goal.GoalType == "Header")
+                        goal.isHeader = true;
+                    else if(goal.GoalType == "Shot")
+                        goal.isShot = true;
+                    else if(goal.GoalType == "Penalty")
+                        goal.isPenalty = true;
+                    else if(goal.GoalType == "Free Kick")
+                        goal.isFreeKick = true;
+                    else
+                        goal.isUnknown= true;
+                } else {
+                    goal.isAssist = true;
+                    if(goal.AssistType == "Header")
+                        goal.isHeader = true;
+                    else if(goal.AssistType == "Cross")
+                        goal.isCross = true;
+                    else if(goal.AssistType == "Pass")
+                        goal.isPass = true;
+                    else if(goal.AssistType == "Free Kick")
+                        goal.isFreeKick = true;
+                    else if(goal.AssistType == "Corner")
+                        goal.isCorner = true;
+                    else
+                        goal.isUnknown= true;
+                }
+                goalsList.push(goal);
+            }
+
+            return Promise.resolve(goalsList);
+         },
+
+         calculateStats : function(apps, goals) {
+
+            var seasons = [];
+            var total = {
+                apps: 0,
+                starts: 0,
+                subs: 0,
+                goals: 0,
+                assists: 0,
+                headers: 0,
+                fk: 0,
+                penalties: 0,
+                yellow: 0,
+                red: 0,
+            };
+            currentObj = {Season:0};
+            for(var i=0; i < apps.length; i++) {
+                if(apps[i].Season > currentObj.Season) {
+                    if(currentObj.Season > 0) {
+                        currentObj = this.calculateGoalsforSeason(currentObj, goals);
+                        seasons.push(currentObj);
+                        total = this.incrementPlayerTotals(total, currentObj);
+                    }
+                    currentObj = {
+                        Season: apps[i].Season,
+                        apps: 1,
+                        starts: apps[i].isSub ? 0 : 1,
+                        subs: apps[i].isSub ? 1 : 0,
+                        yellow: apps[i].yellow ? 1: 0,
+                        red: apps[i].red ? 1: 0,
+                        goals: 0,
+                        assists: 0,
+                        headers: 0,
+                        fk: 0,
+                        penalties: 0
+                    };
+                } else {
+                    currentObj.apps = currentObj.apps + 1;
+                    currentObj.starts = apps[i].isSub ? currentObj.starts : currentObj.starts +1;
+                    currentObj.subs = apps[i].isSub ? currentObj.subs + 1 : currentObj.subs;
+                    currentObj.yellow = apps[i].yellow ? currentObj.yellow + 1 : currentObj.yellow;
+                    currentObj.red = apps[i].red ? currentObj.red + 1 : currentObj.red;
+                }
+            }
+            if(currentObj.Season > 0) {
+                currentObj = this.calculateGoalsforSeason(currentObj, goals);
+                seasons.push(currentObj);
+                total = this.incrementPlayerTotals(total, currentObj);
+            }
+
+            return {seasons:seasons, total: total};
+         },
+
+         incrementPlayerTotals: function (total, currentObj) {
+             total.apps = total.apps + 1;
+             total.starts = total.starts + currentObj.starts;
+             total.subs = total.subs + currentObj.subs;
+             total.yellow = total.yellow + currentObj.yellow;
+             total.red = total.red + currentObj.red;
+             total.goals = total.goals + currentObj.goals;
+             total.assists = total.assists + currentObj.assists;
+             total.fk = total.fk + currentObj.fk;
+             total.headers = total.headers + currentObj.headers;
+             total.penalties = total.penalties + currentObj.penalties;
+             return total;
+         },
+
+         calculateGoalsforSeason : function(currentObj, goals) {
+             for(var x=0; x < goals.length; x++) {
+                 if(currentObj.Season == goals[x].Season) {
+                     currentObj.goals = goals[x].isGoal ? currentObj.goals + 1 : currentObj.goals;
+                     currentObj.assists = goals[x].isAssist ? currentObj.assists + 1 : currentObj.assists;
+                     if(goals[x].isGoal) {
+                         currentObj.headers = goals[x].isHeader ? currentObj.headers + 1 : currentObj.headers;
+                         currentObj.fk = goals[x].isFreeKick ? currentObj.fk + 1 : currentObj.fk;
+                         currentObj.penalties = goals[x].isPenalty ? currentObj.penalties + 1 : currentObj.penalties;
+                     }
+                 }
+             }
+             return currentObj;
+         },
+
+         findAppsByPlayer : async function(player, size, season) {
+            var query = {
+                index: "apps",
+                body: {
+                   "sort": ["Date"],
+                   "size": 200,
+                   "query": {
+                     "bool": {
+                        "must": [
+                          {
+                              "match": {
+                                "PlayersInvolved": player
+                              }
+                          }
+                        ]
+                      }
+                   }
+                }
+              };
+
+            if(season) {
+                query.body.query.bool.must.push(
+                    {
+                        "match": {
+                            "Season": season
+                        }
+                    }
+                );
+            }
+
+            var apps = await client.search(query);
+            var appsList = [];
+            for(var i=0; i < apps.body.hits.hits.length; i++){
+                var app = apps.body.hits.hits[i]["_source"];
+                if(app.Name != player) {
+                    app.isSub = true;
+                    app.replaced = app.Name;
+                    app.yellow = app.SubYellow;
+                    app.red = app.SubRed;
+                } else {
+                    app.replaced = app.SubbedBy;
+                    app.yellow = app.YellowCard;
+                    app.red = app.RedCard;
+                }
+                appsList.push(app);
+            }
+
+            return Promise.resolve(appsList);
+         },
+
          findAllTeams : async function(size) {
             var teamQuery = {
                 index: "matches",
@@ -96,7 +293,38 @@ module.exports = function (path, fs, Mustache,client) {
                 }
             };
 
-            return await client.search(teamQuery);
+            var result = await client.search(teamQuery);
+            var results = [];
+            var resultsByLetter = {};
+            for(var i=0; i < result.body.aggregations.teams.buckets.length; i++) {
+                if(result.body.aggregations.teams.buckets[i].key != "Tranmere Rovers") {
+
+                    var firstLetter = result.body.aggregations.teams.buckets[i].key.substring(0,1);
+                    if(resultsByLetter[firstLetter]) {
+                        resultsByLetter[firstLetter].push(result.body.aggregations.teams.buckets[i].key)
+                    } else {
+                        var obj = [result.body.aggregations.teams.buckets[i].key];
+                        resultsByLetter[firstLetter] = obj;
+                    }
+                    results.push(result.body.aggregations.teams.buckets[i])
+                }
+            }
+            var list = [];
+            var keys = Object.keys(resultsByLetter);
+            for(var i=0; i < keys.length; i++) {
+                list.push({key: keys[i], teams: resultsByLetter[keys[i]]});
+            }
+            results.sort(function(a, b) {
+              if (a.key < b.key) return -1;
+              if (a.key > b.key) return 1;
+              return 0;
+            });
+            list.sort(function(a, b) {
+              if (a.key < b.key) return -1;
+              if (a.key > b.key) return 1;
+              return 0;
+            });
+            return {results:results, resultsByLetter:list};
          },
 
          findAllTranmereMatchesByOpposition : async function(opposition, size) {
@@ -123,7 +351,29 @@ module.exports = function (path, fs, Mustache,client) {
                    }
                 }
               };
-         return await client.search(query);
+            return await client.search(query);
+         },
+
+         getLinksByPlayer : async function(name, size) {
+              var query = {
+                index: "links",
+                body: {
+                   "size": size,
+                   "query": {
+                     "bool": {
+                        "must": [
+                          {
+                            "match": {
+                             "Name" : name
+                            }
+                          }
+                        ]
+                      }
+                   }
+                }
+              };
+
+             return client.search(query);
          },
 
          findAllTranmereMatchesBySeason : async function(season, size) {
