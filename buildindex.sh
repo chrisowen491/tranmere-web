24214583381f808504b209d945677cce7e7c007c
chrisowen <chrisowen491@gmail.com>
2020-07-20 11:31:27 +0100

    Updates

---
+++ buildindex.sh
@@ -0,0 +1,27 @@
+npm run export-excel
+curl -i -X DELETE http://localhost:9200/apps -H "Content-Type: application/json"
+curl -i -X PUT http://localhost:9200/apps -H "Content-Type: application/json" --data-binary "@data/mappings/apps.mapping"
+curl -i -X DELETE http://localhost:9200/goals -H "Content-Type: application/json"
+curl -i -X PUT http://localhost:9200/goals -H "Content-Type: application/json" --data-binary "@data/mappings/goals.mapping"
+curl -i -X DELETE http://localhost:9200/players -H "Content-Type: application/json"
+curl -i -X PUT http://localhost:9200/players -H "Content-Type: application/json" --data-binary "@data/mappings/players.mapping"
+curl -i -X DELETE http://localhost:9200/programmes -H "Content-Type: application/json"
+curl -i -X PUT http://localhost:9200/programmes -H "Content-Type: application/json" --data-binary "@data/mappings/programmes.mapping"
+curl -i -X DELETE http://localhost:9200/managers -H "Content-Type: application/json"
+curl -i -X PUT http://localhost:9200/managers -H "Content-Type: application/json" --data-binary "@data/mappings/managers.mapping"
+curl -i -X DELETE http://localhost:9200/teams -H "Content-Type: application/json"
+curl -i -X PUT http://localhost:9200/teams -H "Content-Type: application/json" --data-binary "@data/mappings/teams.mapping"
+curl -i -X DELETE http://localhost:9200/matches -H "Content-Type: application/json"
+curl -i -X PUT http://localhost:9200/matches -H "Content-Type: application/json" --data-binary "@data/mappings/matches.mapping"
+curl -i -X DELETE http://localhost:9200/stars -H "Content-Type: application/json"
+curl -i -X PUT http://localhost:9200/stars -H "Content-Type: application/json" --data-binary "@data/mappings/stars.mapping"
+curl -i -X DELETE http://localhost:9200/transfers -H "Content-Type: application/json"
+curl -i -X PUT http://localhost:9200/transfers -H "Content-Type: application/json" --data-binary "@data/mappings/transfers.mapping"
+curl -i -X DELETE http://localhost:9200/links -H "Content-Type: application/json"
+curl -i -X PUT http://localhost:9200/links -H "Content-Type: application/json" --data-binary "@data/mappings/links.mapping"
+curl -i -X DELETE http://localhost:9200/media -H "Content-Type: application/json"
+curl -i -X PUT http://localhost:9200/media -H "Content-Type: application/json" --data-binary "@data/mappings/media.mapping"
+
+rm /tmp/logstash
+rm -rf /tmp/logstash4/
+logstash -f 'data/*.conf' --log.level warn --path.data /tmp/logstash4 > data/log.txt
--- readme.md
+++ readme.md
@@ -1,28 +1,33 @@
 # Tranmere Web.com
 
+This site is a demo site using Tarnmere Rovers data to demonstrate all sorts of website functionality. 
+It is not meant as a commercial entity, and is purely for fun. 
+
 ## tranmere-web
-Source code for Tranmere-Web.com
+Source code for Tranmere-Web.com. The site is a static site designed to be deployed onto Amazon S3.
+There are a number of NPM scripts which build a local Elastic Search index, and then use this to generate a set of HTML files. 
+
+### Prerequisites
+
+ * Scripts are designed for Linux/Mac
+ * Node, NPM
+ * Elastic search running locally on port 9200 
+
+### Building
+
+npm install
+./data/cleanindex.sh
+npm run build-web
+gulp 
 
 ### Deploying
 
 aws s3 sync ./tranmere-web/output/site s3://www.tranmere-web.com
 aws s3 sync ./images s3://trfc-programmes
-=CONCATENATE("1989-90/Scan ",E24,".jpeg")
-
-
-### ToDo
-
-  * Schema.Org Markup
-  * Player Grouping
-  * Attendances
-  * Better Bio Data for players
-  * Player media
-  * Combined Manager Stats
-  * More Links
-  * Check Last Games Of Season 
   
-## Tranmere web API
+## tranmere-web-api
+
+The API is a set of AWS Lambdas. 
 
 ###  
-  
-
+./deploy-api.sh  
