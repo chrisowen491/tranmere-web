a6db3b64a88ac17c8ecece5aee821bfa8e5b87e4
chrisowen <chrisowen491@gmail.com>
2020-10-04 18:39:31 +0100

    Update

--- tranmere-web/assets/js/player.js
+++ tranmere-web/assets/js/player.js
@@ -1,15 +1,70 @@
 $(document).ready(function() {
 
-  var url = 'https://www.tranmere-web.com/api-player/'+ $("#PlayerName").html();
+var formatter = new Intl.NumberFormat('en-GB', {
+  style: 'currency',
+  currency: 'GBP',
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0
+});
 
+  var url = 'https://www.tranmere-web.com/entities/TranmereWebPlayerTable/name/'+ $("#PlayerName").html() +"?v=1";
 
   $.getJSON(url, function(data) {
-    if(data.length == 1) {
+    if(data.message.length == 1) {
         $.get("/assets/templates/player.mustache", function(template) {
-          var article = Mustache.render( template, data[0] );
+          var article = Mustache.render( template, data.message[0] );
           $("#profile").html(article);
         });
     }
+  });
+
+  var linksUrl = 'https://www.tranmere-web.com/entities/TranmereWebPlayerLinks/name/'+ $("#PlayerName").html() +"?v=1";
+
+  $.getJSON(linksUrl, function(data) {
+    $.get("/assets/templates/links.mustache", function(template) {
+      var article = Mustache.render( template, data );
+      $("#pnlLinks").html(article);
+    });
+  });
+
+  var transfersUrl = 'https://www.tranmere-web.com/entities/TranmereWebPlayerTransfers/name/'+ $("#PlayerName").html() +"?v=1";
+
+  $.getJSON(transfersUrl, function(data) {
+
+     var transfers = [];
+     for(var i=0; i < data.message.length; i++) {
+       var transfer = data.message[i];
+       if(transfer.direction == "In")
+        transfer.incoming = true;
+       if(transfer.type == "Loan" || transfer.type == "Youth" || transfer.type == "FreeTransfer" || transfer.type == "Retired" || transfer.type == "Released")
+        transfer.noFee = true;
+       if(transfer.type == "Loan" || transfer.type == "Youth" || transfer.type == "FreeTransfer" || transfer.type == "Tribunal" || transfer.type == "Retired" || transfer.type == "Released")
+        transfer.other = true;
+       if(transfer.type == "Youth" )
+         transfer.type = "Tranmere Youth Team"
+       transfers.push(transfer)
+     }
+
+
+    var view = {
+        transfers: transfers,
+        currencyFormat: function() {
+            return function(text, render) {
+              if(render(text) == "") {
+                return "Undisclosed"
+              }
+              else if(parseInt(render(text)) == 0)
+                return "Free Transfer"
+              else {
+                return formatter.format(parseInt(render(text)))
+              }
+            }
+        }
+    }
+    $.get("/assets/templates/transfers.mustache", function(template) {
+      var article = Mustache.render( template, view );
+      $("#pnlTransfers").html(article);
+    });
   });
 
 });
