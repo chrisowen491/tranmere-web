98f3df33cc98b2b4f1c8e70e51f1f0d13abf940e
chrisowen <chrisowen491@gmail.com>
2021-03-22 17:24:19 +0000

    Update

--- tranmere-web/assets/js/player-search.js
+++ tranmere-web/assets/js/player-search.js
@@ -1,11 +1,44 @@
+var seasonMapping = {
+    "1978": 1977,
+    "1984": 1983,
+    "1990": 1989,
+    "1992": 1991,
+    "1994": 1993,
+    "1996": 1995,
+    "1998": 1997,
+    "2001": 2000,
+    "2003": 2002,
+    "2005": 2004,
+    "2008": 2007
+}
+
+
 function search() {
   $("#loading").show();
   $("#content").hide();
   var base = window.location.hostname == "www.tranmere-web.com" ? '' : "https://www.tranmere-web.com";
   var url = base + '/player-search/?season='+ $('#season').val()
               + '&sort=' + $('#sort').val();
+  var re = /\/\d\d\d\d\//gm;
+  var re2 = /\/\d\d\d\dgk\//gm;
+  var re3 = /\/\d\d\d\d[A-Za-z]\//gm;
   $.getJSON(url, function(view) {
     $.get("/assets/templates/players.mustache", function(template) {
+
+      if($('#season').val() && $('#season').val() > 1976) {
+          for(var i=0; i < view.players.length; i++) {
+            if(view.players[i].bio && view.players[i].bio.picLink) {
+                var season = $('#season').val()
+                if(seasonMapping[season])
+                    season = seasonMapping[season]
+                view.players[i].bio.picLink = view.players[i].bio.picLink.replace(re, '/'+season+'/')
+                view.players[i].bio.picLink = view.players[i].bio.picLink.replace(re3, '/'+season+'/')
+                console.log(view.players[i].bio.picLink)
+            }
+          }
+      }
+
+
       var article = Mustache.render( template, view );
       $("#content").html(article);
       $("#loading").hide();
