bcfe472044a886f8ca21903ff651b553fd9ccf1d
chrisowen491 <chrisowen491@gmail.com>
2022-05-07 19:20:38 +0100

    Fix Request

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -10,7 +10,6 @@ export async function onRequest(context) {
     data, // arbitrary space for passing data between middlewares
   } = context;
 
-  const request = context.request
   const cacheUrl = new URL(request.url)
 
   const cacheKey = new Request(cacheUrl.toString(), request)
@@ -21,7 +20,7 @@ export async function onRequest(context) {
 
   if (!response) {
 
-      let url = new URL(context.request.url)
+      let url = new URL(request.url)
 
       if(url.pathname === "/" || url.pathname.startsWith("/page")) {
         url.host = "api.ci1.tranmere-web.com"
@@ -34,17 +33,17 @@ export async function onRequest(context) {
         url.host = "api.ci1.tranmere-web.com"
       }
 
-      let request = new Request(url, context.request)
+      let new_request = new Request(url, request)
 
       if(url.pathname.startsWith("/graphql")) {
         url.host = "tgzafameebdujmbaw4tdkuaiku.appsync-api.eu-west-1.amazonaws.com";
-        request.headers.set("x-api-key", "da2-uaiu6wz4gjeetknxcdq5bf7bo4")
+        new_request.headers.set("x-api-key", "da2-uaiu6wz4gjeetknxcdq5bf7bo4")
       } else {
-        request.headers.set("x-api-key", APIKEY)
+        new_request.headers.set("x-api-key", APIKEY)
       }
 
     // If not in cache, get it from origin
-    response = await fetch(request)
+    response = await fetch(new_request)
 
     nav_response = await fetch(new Request('https://raw.githubusercontent.com/chrisowen491/tranmere-web/master/tranmere-web/templates/partials/homenav.partial.mustache'))
     let nav_text = await nav_response.text();
@@ -57,7 +56,7 @@ export async function onRequest(context) {
     if(response.status != 500) {
       // Store the fetched response as cacheKey
       // Use waitUntil so computational expensive tasks don"t delay the response
-      context.waitUntil(cache.put(cacheKey, response.clone()))
+      waitUntil(cache.put(cacheKey, response.clone()))
     }
   }
   return response
