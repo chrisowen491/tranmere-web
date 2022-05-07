e90022e3d2ea5e41e513c27883f12492c135c18c
chrisowen491 <chrisowen491@gmail.com>
2022-05-07 20:16:58 +0100

    add debug

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -10,54 +10,59 @@ export async function onRequest(context) {
     data, // arbitrary space for passing data between middlewares
   } = context;
 
-//  const cacheUrl = new URL(request.url)
+  try {
 
-//  const cacheKey = new Request(cacheUrl.toString(), request)
-//  const cache = caches.default
+  //  const cacheUrl = new URL(request.url)
 
-  // Get this request from this zone's cache
-//  let response = await cache.match(cacheKey)
+  //  const cacheKey = new Request(cacheUrl.toString(), request)
+  //  const cache = caches.default
 
-//  if (!response) {
+    // Get this request from this zone's cache
+  //  let response = await cache.match(cacheKey)
 
-      let url = new URL(request.url)
+  //  if (!response) {
 
-      if(url.pathname === "/" || url.pathname.startsWith("/page")) {
-        url.host = "api.ci1.tranmere-web.com"
-        if(url.pathname === "/") {
-            url.pathname = "/page/home/home"
+        let url = new URL(request.url)
+
+        if(url.pathname === "/" || url.pathname.startsWith("/page")) {
+          url.host = "api.ci1.tranmere-web.com"
+          if(url.pathname === "/") {
+              url.pathname = "/page/home/home"
+          }
         }
+
+        if(url.pathname.startsWith("/player-search") || url.pathname.startsWith("/result-search") || url.pathname.startsWith("/builder")|| url.pathname.startsWith("/match") || url.pathname.startsWith("/contact-us")) {
+          url.host = "api.ci1.tranmere-web.com"
+        }
+
+        let new_request = new Request(url, request)
+
+        if(url.pathname.startsWith("/graphql")) {
+          url.host = "tgzafameebdujmbaw4tdkuaiku.appsync-api.eu-west-1.amazonaws.com";
+          new_request.headers.set("x-api-key", "da2-uaiu6wz4gjeetknxcdq5bf7bo4")
+        } else {
+          new_request.headers.set("x-api-key", APIKEY)
+        }
+
+      // If not in cache, get it from origin
+      response = await fetch(new_request)
+
+      nav_response = await fetch(new Request('https://raw.githubusercontent.com/chrisowen491/tranmere-web/master/tranmere-web/templates/partials/homenav.partial.mustache'))
+      let nav_text = await nav_response.text();
+      var amendedBody = await response.text()
+      amendedBody = amendedBody.replace(/NAV_BAR_PLACEHOLDER/g, nav_text);
+
+      // Must use Response constructor to inherit all of response's fields
+      response = new Response(amendedBody, response)
+
+      if(response.status != 500) {
+        // Store the fetched response as cacheKey
+        // Use waitUntil so computational expensive tasks don"t delay the response
+        //waitUntil(cache.put(cacheKey, response.clone()))
       }
-
-      if(url.pathname.startsWith("/player-search") || url.pathname.startsWith("/result-search") || url.pathname.startsWith("/builder")|| url.pathname.startsWith("/match") || url.pathname.startsWith("/contact-us")) {
-        url.host = "api.ci1.tranmere-web.com"
-      }
-
-      let new_request = new Request(url, request)
-
-      if(url.pathname.startsWith("/graphql")) {
-        url.host = "tgzafameebdujmbaw4tdkuaiku.appsync-api.eu-west-1.amazonaws.com";
-        new_request.headers.set("x-api-key", "da2-uaiu6wz4gjeetknxcdq5bf7bo4")
-      } else {
-        new_request.headers.set("x-api-key", APIKEY)
-      }
-
-    // If not in cache, get it from origin
-    response = await fetch(new_request)
-
-    nav_response = await fetch(new Request('https://raw.githubusercontent.com/chrisowen491/tranmere-web/master/tranmere-web/templates/partials/homenav.partial.mustache'))
-    let nav_text = await nav_response.text();
-    var amendedBody = await response.text()
-    amendedBody = amendedBody.replace(/NAV_BAR_PLACEHOLDER/g, nav_text);
-
-    // Must use Response constructor to inherit all of response's fields
-    response = new Response(amendedBody, response)
-
-    if(response.status != 500) {
-      // Store the fetched response as cacheKey
-      // Use waitUntil so computational expensive tasks don"t delay the response
-      //waitUntil(cache.put(cacheKey, response.clone()))
-    }
-  //}
-  return response
+    //}
+    return response
+  } catch (err) {
+    return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+  }
 }
