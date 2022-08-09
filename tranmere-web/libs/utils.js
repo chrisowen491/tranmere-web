a192a1ee673cb964a04bdd44249f251a20a94da2
chrisowen491 <chrisowen491@gmail.com>
2022-08-09 22:42:33 +0100

    Update

--- tranmere-web/libs/utils.js
+++ tranmere-web/libs/utils.js
@@ -156,7 +156,8 @@ module.exports = function (path, fs, Mustache, axios, key) {
             for(var i= 1977; i <= theYear; i++) {
                 var result = await axios.get("https://api.ci1.tranmere-web.com/player-search/?season="+i+"&sort=Goals", this.apiOptions);
                 var player = result.data.players[0];
-                results.push(player);
+                if(player)
+                  results.push(player);
             }
             return results;
          },
