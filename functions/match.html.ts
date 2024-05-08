73039702c87e5d35a847a77696415773d99fc4cc
chrisowen491 <chrisowen491@gmail.com>
2024-05-08 21:25:20 +0100

    Spike

---
+++ functions/match.html.ts
@@ -0,0 +1,13 @@
+import { CloudflareEnv } from '../lib/tranmere-web-types';
+
+export const onRequest: PagesFunction<CloudflareEnv> = async (context) => {
+  const url = new URL(context.request.url);
+  if(url.searchParams.get("season") && url.searchParams.get("date")) {
+    const destinationURL = `https://www.tranmere-web.com/match/${url.searchParams.get("season")}/${url.searchParams.get("date")}`;
+    const statusCode = 301;
+    return Response.redirect(destinationURL, statusCode);
+  }
+  else {
+    return await fetch(context.request);
+  }
+};
