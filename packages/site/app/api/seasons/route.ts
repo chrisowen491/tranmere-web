4ae2fcc42c6d7c398c68844171b2a557ab4bca08
chrisowen491 <chrisowen491@gmail.com>
2024-07-23 20:50:53 +0100

    add seasons

---
+++ packages/site/app/api/seasons/route.ts
@@ -0,0 +1,17 @@
+export const runtime = "edge";
+
+import { NextRequest, NextResponse } from "next/server";
+import { GetSeasons } from "@/lib/apiFunctions";
+
+//export interface Season
+
+export async function POST(req: NextRequest) {
+ 
+    const seasons = GetSeasons();
+    return NextResponse.json(seasons.map(s => {
+        return {
+            value: `${s}-${s+1}`,
+            label: `${s}-${s+1}`
+        }
+    }), { status: 200 });
+}
