41f7787c11a1e9f2e8ee3df38422eab80dffe92e
Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
2024-09-06 23:49:15 +0000

    ci: version bump to v3.0.213

--- packages/api-stack/lib/tranmere-web-graphql.ts
+++ packages/api-stack/lib/tranmere-web-graphql.ts
@@ -103,7 +103,7 @@ export class TranmereWebGraphQL extends Construct {
           ]
         }
       );
-      
+
       graph_ql.addMethod(
         'PUT',
         new apigw.AwsIntegration({
@@ -133,7 +133,6 @@ export class TranmereWebGraphQL extends Construct {
           ]
         }
       );
-      
     }
 
     if (props.tables) {
