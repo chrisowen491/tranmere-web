c4df78388df6c4ab9c0252ee8dafdb66bcc93780
chrisowen491 <chrisowen491@gmail.com>
2024-10-11 21:07:28 +0100

    update

--- packages/site/components/carousel/Slider.tsx
+++ packages/site/components/carousel/Slider.tsx
@@ -5,36 +5,37 @@ const Slider = (props: { images: GalleryImage[]; title: string }) => {
 
   return (
     <>
-    <div>
-      <div className="mx-auto max-w-2xl px-4 py-2 sm:px-6 sm:py-2 lg:max-w-7xl lg:px-8">
-
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {images.map((elem, idx) => (
-            <div key={idx} className="group relative">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-indigo-200 dark:bg-black lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <img
-                  alt={elem.description}
-                  src={elem.url}
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700 dark:text-indigo-50">
-                    <a href={elem.url}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {elem.title}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500 dark:text-indigo-50">{elem.description}</p>
+      <div>
+        <div className="mx-auto max-w-2xl px-4 py-2 sm:px-6 sm:py-2 lg:max-w-7xl lg:px-8">
+          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {images.map((elem, idx) => (
+              <div key={idx} className="group relative">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-indigo-200 dark:bg-black lg:aspect-none group-hover:opacity-75 lg:h-80">
+                  <img
+                    alt={elem.description}
+                    src={elem.url}
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-700 dark:text-indigo-50">
+                      <a href={elem.url}>
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {elem.title}
+                      </a>
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-500 dark:text-indigo-50">
+                      {elem.description}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  </>
+    </>
   );
 };
 
