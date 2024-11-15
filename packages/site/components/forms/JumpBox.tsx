0fcdba32bd0c7e5d0b562560e83c8e68dbd721aa
chrisowen491 <chrisowen491@gmail.com>
2024-11-15 22:22:16 +0000

    Update

--- packages/site/components/forms/JumpBox.tsx
+++ packages/site/components/forms/JumpBox.tsx
@@ -8,7 +8,7 @@ export function JumpBox(props: { season: string, seasons: number[] }) {
             id="switch"
             name="switch"
             defaultValue={props.season}
-            onChange={(e) => { location.href = "/season/" + e.target.value }}
+            onChange={(e) => { location.href = "/season/" + parseInt(e.target.value) }}
             className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm/6"
         >
         { props.seasons.map((season) => (
