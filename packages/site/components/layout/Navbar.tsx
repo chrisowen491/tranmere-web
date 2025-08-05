7c07e25eee37c0dda3abab2702457934dc1f1a15
chrisowen491 <chrisowen491@gmail.com>
2025-08-05 11:56:35 +0100

    updates

--- packages/site/components/layout/Navbar.tsx
+++ packages/site/components/layout/Navbar.tsx
@@ -17,7 +17,7 @@ import Image from "next/image";
 
 const navigation = [
   { name: "Results", href: "/results", current: false },
-  { name: "Seasons", href: "/season/2024", current: false },
+  { name: "Seasons", href: "/season/2025", current: false },
   { name: "Players", href: "/playersearch", current: false },
   { name: "Gallery", href: "/page/tag/Gallery", current: false },
   { name: "Blogs & Articles", href: "/blog", current: false },
@@ -27,7 +27,7 @@ const navigation = [
 const mobilenavigation = [
   { name: "Home", href: "/", current: false },
   { name: "Results", href: "/results", current: false },
-  { name: "Seasons", href: "/season/2024", current: false },
+  { name: "Seasons", href: "/season/2025", current: false },
   { name: "Players", href: "/playersearch", current: false },
   { name: "Transfers", href: "/transfer-central", current: false },
   { name: "Blogs & Articles", href: "/blog", current: false },
