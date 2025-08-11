f7099900582292396b9b142e45fff35aba71a6c6
chrisowen491 <chrisowen491@gmail.com>
2025-08-11 12:37:37 +0100

    chore: update agent

--- packages/tools/src/LineupsTool.ts
+++ packages/tools/src/LineupsTool.ts
@@ -26,7 +26,6 @@ export const LineupsTool = tool({
   execute: async ({ date, opposition, competition, season, homeTeam }) => {
     const day = moment(date).format('YYYY-MM-DD');
 
-    console.log(day);
     const fixtureUrl = `https://www.bbc.co.uk/wc-data/container/sport-data-scores-fixtures?selectedEndDate=${day}&selectedStartDate=${day}&todayDate=${day}&urn=urn%3Abbc%3Asportsdata%3Afootball%3Ateam%3Atranmere-rovers&useSdApi=false`;
     const fixturesResponse = await fetch(fixtureUrl, STANDARD_HEADERS);
     const fixtures = (await fixturesResponse.json()) as FixtureSet;
@@ -36,6 +35,7 @@ export const LineupsTool = tool({
       const lineupResponse = await fetch(lineup_url, STANDARD_HEADERS);
       const lineups = (await lineupResponse.json()) as TeamLineups;
 
+
       const team = homeTeam === 'Tranmere Rovers' ? 'homeTeam' : 'awayTeam';
       const apps: Appearance[] = [];
       for await (const element of lineups[team].players.starters) {
@@ -44,40 +44,45 @@ export const LineupsTool = tool({
             s.substitutedOn &&
             s.substitutedOn.playerOffName === element.displayName
         );
-        const app: Appearance = {
-          id: uuidv4(),
-          Date: day!,
-          Opposition: opposition!,
-          Competition: competition!,
-          Season: season,
-          Name: translatePlayerName(
-            utils.simplifyName(element.name.first) + ' ' + element.name.last
-          ),
-          Number: element.shirtNumber.toString(),
-          SubbedBy: sub
-            ? translatePlayerName(
-                utils.simplifyName(sub.name.first) + ' ' + sub.name.last
-              )
-            : null,
-          SubTime: sub ? sub.substitutedOn?.timeMin.toString() : null,
-          YellowCard: element.cards.find((el) => el.type === 'Yellow Card')
-            ? 'TRUE'
-            : null,
-          RedCard: element.cards.find((el) => el.type === 'Red Card')
-            ? 'TRUE'
-            : null,
-          SubYellow:
-            sub && sub.cards.find((el) => el.type === 'Yellow Card')
+        try{
+          const app: Appearance = {
+            id: uuidv4(),
+            Date: day!,
+            Opposition: opposition!,
+            Competition: competition!,
+            Season: season,
+            Name: translatePlayerName(
+              utils.simplifyName(element.name.first) + ' ' + element.name.last
+            ),
+            Number: element.shirtNumber.toString(),
+            SubbedBy: sub
+              ? translatePlayerName(
+                  utils.simplifyName(sub.name.first) + ' ' + sub.name.last
+                )
+              : null,
+            SubTime: sub ? sub.substitutedOn?.timeMin.toString() : null,
+            YellowCard: element.cards.find((el) => el.type === 'Yellow Card')
               ? 'TRUE'
               : null,
-          SubRed:
-            sub && sub.cards.find((el) => el.type === 'Red Card')
+            RedCard: element.cards.find((el) => el.type === 'Red Card')
               ? 'TRUE'
-              : null
-        };
-        if (!sub) delete app.SubbedBy;
-        apps.push(app);
+              : null,
+            SubYellow:
+              sub && sub.cards.find((el) => el.type === 'Yellow Card')
+                ? 'TRUE'
+                : null,
+            SubRed:
+              sub && sub.cards.find((el) => el.type === 'Red Card')
+                ? 'TRUE'
+                : null
+          };
+          if (!sub) delete app.SubbedBy;
+          apps.push(app);
+        } catch (e) {
+          console.log('Error processing player:', element, e);
+        }
       }
+
       return JSON.stringify(apps);
     } else {
       return 'Could not find the fixture';
