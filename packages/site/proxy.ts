ca72baeda15f564f33ed9a82cebe93d7d84fda2e
chrisowen491 <chrisowen491@gmail.com>
2026-08-09 21:24:56 +0100

    chore: update

--- packages/site/proxy.ts
+++ packages/site/proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse, type NextRequest } from "next/server";
 
 import { auth0 } from "./lib/auth0";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const authRes = await auth0.middleware(request);
 
   if (request.nextUrl.pathname.startsWith("/api/comment")) {
