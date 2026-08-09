1f78303d689531997bf7e18a14f865054ed44cc3
chrisowen491 <chrisowen491@gmail.com>
2026-08-09 19:59:53 +0100

    chore: update

--- packages/api-stack/lib/tranmere-web-stack.ts
+++ packages/api-stack/lib/tranmere-web-stack.ts
@@ -3,24 +3,13 @@ import { aws_apigateway as apigw } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as ddb from 'aws-cdk-lib/aws-dynamodb';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import { TranmereWebLambda } from './tranmere-web-lambda';
 import { TranmereWebGraphQL } from './tranmere-web-graphql';
-
-const EMAIL_ADDRESS = process.env.EMAIL_ADDRESS!;
-const DD_TAGS = process.env.DD_TAGS!;
 
 export class TranmereWebStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const rootDomain = 'tranmere-web.com';
-
-    const env_variables = {
-      EMAIL_ADDRESS: EMAIL_ADDRESS,
-      DD_TAGS: DD_TAGS,
-      DD_EXTENSION_VERSION: 'next'
-    };
 
     const TranmereWebHatTricks = ddb.Table.fromTableAttributes(
       this,
@@ -46,20 +35,6 @@ export class TranmereWebStack extends cdk.Stack {
         ),
         endpointType: apigw.EndpointType.EDGE
       }
-    });
-
-    const contact_us = api.root.addResource('contact-us');
-
-    new TranmereWebLambda(this, 'ContactUsFunction', {
-      environment: env_variables,
-      lambdaFile: './lambda/contactus.ts',
-      apiResource: contact_us,
-      apiMethod: 'POST',
-      policy: new iam.PolicyStatement({
-        actions: ['ses:SendEmail', 'SES:SendRawEmail'],
-        resources: ['*'],
-        effect: iam.Effect.ALLOW
-      })
     });
 
     new TranmereWebGraphQL(this, 'TranmereWebGraphQL', {
