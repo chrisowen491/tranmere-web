26ce11a10becaa1526175750822d32e3853a994c
chrisowen491 <chrisowen491@gmail.com>
2026-08-18 20:14:51 +0100

    chore: update

--- packages/site/env.d.ts
+++ packages/site/env.d.ts
@@ -5,10 +5,6 @@ interface CloudflareEnv {
     CONTACT_EMAIL: SendEmail;
     CF_SPACE: string;
     CF_KEY: string;
-    API_PORT: string;
-    API_PROTOCOL: string;
-    API_DOMAIN: string;
-    API_KEY: string;
     AUTH0_BASE_URL: string;
     AUTH0_ISSUER_BASE_URL: string;
     AUTH0_CLIENT_ID: string;
